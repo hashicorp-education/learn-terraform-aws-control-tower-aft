4087a9c2e75a56d05b7d2168aed60cafde35ea01
Yoimer <ydrf19@gmail.com>
2024-06-08 01:19:27 -0400

    initial configuration

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+terraform {
+  backend "s3" {
+    region = "us-east-1"                     # aft main region
+    bucket = "terraform-states-058264455607" # s3 bucket name
+    key    = "aft-setup.tfstate"
+  }
+}
+
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                      = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -11,8 +19,8 @@ module "aft" {
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_request_repo_name                     = "${var.github_username}/mspv2-aft-account-request-core"
+  account_provisioning_customizations_repo_name = "${var.github_username}/mspv2-aft-account-provisioning-customizations-core"
+  global_customizations_repo_name               = "${var.github_username}/mspv2-aft-global-customizations-core"
+  account_customizations_repo_name              = "${var.github_username}/mspv2-aft-account-customizations-core"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "058264455607"
+log_archive_account_id      = "590183664946"
+audit_account_id            = "211125409420"
+aft_management_account_id   = "637423428355"
+github_username             = "aft08062024"
+ct_home_region              = "us-east-1"
+tf_backend_secondary_region = "us-west-2"
